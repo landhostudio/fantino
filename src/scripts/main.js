2df2eddeed9b22960663d22b0ab1fddf142bfeb1
h0r0man <roman.horokhovatskyy@gmail.com>
2017-05-08 10:56:10 +0200

    Added jQuery and nav function

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,22 @@
-console.log('fantino');
+(function($) {
+
+  'use strict';
+  
+  var start = function() {
+
+    if ($('.nav').length) {
+      startNav();
+    };
+
+  };
+
+  function startNav() {
+    $('.nav-button').click(function(event) {
+      $(this).toggleClass('nav-button--opened');
+      $('.nav').toggleClass('nav--opened');
+    });
+  };
+
+  start();
+
+})(jQuery);
